2da983120b30e29dd3545f158fe21df1ba049015
Md Ariful Islam Babu <83685793+Ariful5469@users.noreply.github.com>
2025-03-24 16:22:17 +0600

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -26,7 +26,7 @@ function Signup() {
             return handleError('Name, email, and password are required');
         }
         try {
-            const url = `https://deploy-my-app-api.onrender.com//auth/signup`;
+            const url = `https://deploy-my-app-api.onrender.com/auth/signup`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
