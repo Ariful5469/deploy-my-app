b779d4346c560407e0ae7e9bebace945105f211d
Md Ariful Islam Babu <83685793+Ariful5469@users.noreply.github.com>
2025-03-25 11:07:45 +0600

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -30,7 +30,7 @@ function Home() {
 
   const fetchProducts = async () => {
     try {
-      const url = "https://deploy-my-app-api.onrender.com/products";
+      const url = "https://deploy-mern-app-1-api.vercel.app/products";
       const headers = {
         headers: {
           'Authorization': localStorage.getItem('token')
